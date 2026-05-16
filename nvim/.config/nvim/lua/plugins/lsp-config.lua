e148ab22ad906656b5d4d116480ad916001b18ea
Omarf8 <omarfigueroa223@gmail.com>
2026-05-16 17:10:49 -0500

    keybind lsp diagnostic message

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -23,6 +23,7 @@ return { -- Language Server Protocol
 					vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
 					vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
 					vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+					vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
 					vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
 					vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
 					vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -7,4 +7,4 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 
 -- Keymaps
-vim.keymap.set('i', 'jk', '<Esc>', { desc = 'Exit insert mode' })
+vim.keymap.set('i', 'jk', '<Esc>', { desc = 'Exit insert mode' }) -- Unused, I mapped caps-lock to Esc and use it instead 
