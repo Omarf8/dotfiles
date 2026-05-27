cb7be3c7e86f8bee693f949527cd58d28443351a
Omarf8 <omarfigueroa223@gmail.com>
2026-05-27 10:54:26 -0500

    add comment plugin for jsx support

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -1,4 +1,8 @@
 return { -- Comment
 	'numToStr/Comment.nvim',
-	opts = {}
+	config = function()
+		require("Comment").setup({
+			pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook() -- Allow React support
+		})
+	end
 }
